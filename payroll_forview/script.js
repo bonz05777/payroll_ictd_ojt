f2428a10916820841e6e4987ccde6ce384cbfc30
ictdojtstudents <ictdojtstudents@gmail.com>
2025-03-04 07:21:44 +0000

    requestview

--- payroll_forview/script.js
+++ payroll_forview/script.js
@@ -3,3 +3,4 @@ script.js
 document.getElementById('payroll-period').innerText = 'January 1 - 31, 2025';
 document.getElementById('sheet-no').innerText = '1';
 document.getElementById('total-sheets').innerText = '1';
+
